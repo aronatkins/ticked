275be5990c4867bda57fbd208210dc3ccdd05c12
Aron Atkins <aron@gweep.net>
2012-03-12 00:07:02 -0400

    simple data sender, a graph, and bucketed summaries.

---
+++ sender.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import socket
+import time
+import random
+
+a = [ 'yankee', 'hotel', 'foxtrot' ]
+b = [ 'alpha', 'bravo', 'charley', 'delta' ]
+
+udp_ip="127.0.0.1"
+udp_port=8025
+
+sleep_range = range(0,250)
+sock = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+try :
+    while True :
+        m = '%s:%s' % (random.choice(a), random.choice(b))
+        sock.sendto( m, (udp_ip, udp_port) )
+        time.sleep(random.choice(sleep_range) / 1000.0)
+finally :
+    sock.close()
--- ticked.js
+++ ticked.js
@@ -7,8 +7,8 @@ var fs = require('fs');
 
 // begin config.
 var config = {
-    buckets : 10,
-    bucket_interval : 60000,
+    buckets : 60,
+    bucket_interval : 1000,
 
     receiver_http_address : undefined,
     receiver_http_port    : 8020,
@@ -19,7 +19,7 @@ var config = {
     server_http_address  : undefined,
     server_http_port     : 8080,
 
-    push_interval : 1000
+    push_interval : 1000,
 };
 // end config.
 
@@ -55,9 +55,14 @@ function get_bucket(ts) {
 	    return last;
 	}
 
-	// nope. add one.
-	bucket = create_bucket(ts);
-	buckets.push(bucket);
+	// nope. add some
+	var min_ts = ts - (config.bucket_interval * config.buckets);
+	var ts_to_add = Math.max(min_ts, last.ts + config.bucket_interval);
+	while (ts_to_add <= ts) {
+	    bucket = create_bucket(ts_to_add);
+	    buckets.push(bucket);
+	    ts_to_add += config.bucket_interval;
+	}
 
 	// make sure that we don't have too many.
 	while (buckets.length > config.buckets) {
@@ -65,7 +70,6 @@ function get_bucket(ts) {
 	}
 
 	// make sure we our data isn't too old.
-	min_ts = ts - (config.bucket_interval * config.buckets);
 	while (buckets[0].ts < min_ts) {
 	    buckets.shift();
 	}
@@ -104,16 +108,16 @@ var udp_receiver = dgram.createSocket('udp4', function(msg,rinfo) {
 
     var parts = msg.toString().split(':');
     var pub_id = parts.shift();
+    // sometimes we get undefined shifting off here.
     var value  = parts.shift().replace(/[^a-zA-Z_\-0-9\.]/g, '');
 
     record(pub_id, value);
 });
 
-// index file is tiny. read it in once.
-var index = fs.readFileSync(__dirname + '/public/index.html');
-
 // Create the HTTP server for the clients.
 var server = http.createServer(function (request, response) {
+    var index = fs.readFileSync(__dirname + '/public/index.html');
+
     response.writeHead(200, {'Content-Type' : 'text/html'});
     response.end(index);
 
@@ -139,7 +143,8 @@ var push_interval = setInterval(function () {
     // todo: only push the most recent and have a different way of
     // fetching the historical data at first load.
 
-    io.sockets.volatile.emit("ticker", buckets);
+    // tbd: have different top-level events.
+    io.sockets.volatile.emit("ticked", buckets);
 }, config.push_interval || 10000);
 
 util.log("ready to tick... you off.");
